66a625ca7211e15a0e826056046755aaf749ecc2
Roman Elizarov <elizarov@gmail.com>
2024-12-22 21:51:57 +0300

    Day 22 simpler parallel code

--- src/Day22_2_par.kt
+++ src/Day22_2_par.kt
@@ -1,6 +1,4 @@
 import java.util.concurrent.ForkJoinPool
-import java.util.concurrent.ForkJoinTask
-import java.util.concurrent.RecursiveAction
 import java.util.concurrent.RecursiveTask
 import kotlin.system.measureTimeMillis
 
@@ -24,40 +22,29 @@ fun main() {
         }
     }
     class Task(val i: Int, val pc: IntArray) : RecursiveTask<Int>() {
-        fun computeDirect(): Int {
-            fun find(i: Int): Int {
-                if (i < 4) {
-                    var ans = 0
-                    for (j in -9..9) {
-                        pc[i] = j
-                        ans = maxOf(ans, find(i + 1))
-                    }
-                    return ans
-                }
-                var sum = 0
-                for (si in 0..<a.size) {
-                    for (pi in 3..<2000) {
-                        var ok = true
-                        for (j in 0..3) {
-                            if (pc[j] != pd[si][pi - j]) {
-                                ok = false
-                                break
-                            }
-                        }
-                        if (ok) {
-                            sum += ps[si][pi]
+        fun computeResult(): Int {
+            var sum = 0
+            for (si in 0..<a.size) {
+                for (pi in 3..<2000) {
+                    var ok = true
+                    for (j in 0..3) {
+                        if (pc[j] != pd[si][pi - j]) {
+                            ok = false
                             break
                         }
                     }
+                    if (ok) {
+                        sum += ps[si][pi]
+                        break
+                    }
                 }
-                return sum
             }
-            return find(i)
+            return sum
         }
 
         override fun compute(): Int {
-            if (i >= 2) {
-                return computeDirect()
+            if (i >= 4) {
+                return computeResult()
             }
             val tasks = (-9..9).map { j ->
                 pc[i] = j
