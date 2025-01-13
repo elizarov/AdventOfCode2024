d8cc959c0eb1e991ded66cee3e870c0a6764cdbb
Roman Elizarov <elizarov@gmail.com>
2025-01-13 11:25:20 +0300

    Day1 nicer code

--- src/Day01_1.kt
+++ src/Day01_1.kt
@@ -1,19 +1,11 @@
 import kotlin.math.abs
 
 fun main() {
-    val input = readInput("Day01")
-    val l1 = ArrayList<Long>()
-    val l2 = ArrayList<Long>()
-    for (s in input) {
-        val (a, b) = s.replace(Regex("[ ]+"), " ").split(' ').map { it.toLong() }
-        l1.add(a)
-        l2.add(b)
+    val input = readInput("Day01").map { line ->
+        line.split(' ').filter { it.isNotEmpty() }.map { it.toLong() }
     }
-    var sum = 0L
-    l1.sort()
-    l2.sort()
-    for (i in input.indices) {
-        sum += abs(l1[i] - l2[i])
-    }
+    val l1 = input.map { it[0] }.sorted()
+    val l2 = input.map { it[1] }.sorted()
+    val sum = input.indices.sumOf { i -> abs(l1[i] - l2[i]) }
     println(sum)
 }
